97c757828f67f4a565a26341950ef79bcccfd471
truotaku <najee.s.gardner@gmail.com>
2014-07-11 00:27:24 -0400

    amazon search functionality is fully operational

--- app/models/amazon.rb
+++ app/models/amazon.rb
@@ -1,5 +1,3 @@
-require 'ruby_hmac'
-
 class Amazon
 
   def self.basic_info_hash(response)
@@ -8,29 +6,27 @@ class Amazon
 
   def self.search(search_term)
 
-    api_call = "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJOST6FYFMTXDJ3NA&AssociateTag=giftrcom-20&Condition=All&Keywords=#{URI.escape(search_term)}&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=20140710&Version=2011-08-01"
-
-    HTTParty.get(api_call)
-  end
-
-  def self.search_url(search_term)
-
-    require 'openssl'
-    require 'Base64'
+    iso_time = Time.now.iso8601.split("-")
+    day_ahead = iso_time[2].split("T")
+    iso_time[2] = (day_ahead.first.to_i + 1).to_s + "T" + day_ahead.last
+    iso_time = iso_time[0]+"-"+iso_time[1]+"-"+iso_time[2].gsub(/\:/, "%3A")+".000Z"
 
     key = "5Yhv3A5LCzbBeDmNlzDW4j4fIdhfWNyVFEFF2IFd"
     data = "GET
 webservices.amazon.com
 /onca/xml
-AWSAccessKeyId=AKIAJOST6FYFMTXDJ3NA&AssociateTag=giftrcom-20&Condition=All&Keywords=#{URI.escape(search_term)}&Operation=ItemSearch&ResponseGroup=Images+ItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=#{Time.now.iso8601}&Version=2011-08-01"
+AWSAccessKeyId=AKIAJOST6FYFMTXDJ3NA&AssociateTag=giftrcom-20&Condition=All&Keywords=#{URI.escape(search_term)}&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=#{iso_time}&Version=2011-08-01"
 
-    sig_code = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'), key, data)).strip()
-    sig_code.gsub!(/\+/, "%2")
-    sig_code.gsub!(/\//, "%2")
-    sig_code.gsub!(/\=/, "%3D")
+    sig_code = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), key, data)).strip()
+    # sig_code.gsub!(/\+/, "%2")
+    # sig_code.gsub!(/\//, "%2")
+    # sig_code.gsub!(/\=/, "%3D")
+
+    CGI.escape(sig_code)
 
     data = data.split("\n")
-    "http://"+data[1]+data[2]+"?"+data[3]+"&Signature="+sig_code
 
+    api_call = "http://"+data[1]+data[2]+"?"+data[3]+"&Signature="+sig_code
+    HTTParty.get(api_call)
   end
 end
