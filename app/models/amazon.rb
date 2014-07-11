05b566833043988eddb2a324397e66f9795b1ff9
truotaku <najee.s.gardner@gmail.com>
2014-07-11 15:37:14 -0400

    moved secret keys into environment variables

--- app/models/amazon.rb
+++ app/models/amazon.rb
@@ -1,32 +1,53 @@
 class Amazon
 
-  def self.basic_info_hash(response)
+  def self.search(search_term)
+    amazon_items = amazon_search(search_term)['ItemSearchResponse']['Items']['Item']
 
+    results = Array.new
+
+    amazon_items.each do |item_hash|
+      info = item_hash['ItemAttributes']
+
+      item = Hash.new
+      item[:name] = info['Title']
+      item[:category] = info['Binding'] ? info['Binding'] : "None"
+      item[:price] = info['ListPrice'] ? info['ListPrice']['FormattedPrice'] : "$0.00"
+      item[:small_image_url] = item_hash['SmallImage']['URL']
+      item[:large_image_url] = item_hash['LargeImage']['URL']
+      item[:amazon_url] = item_hash['DetailPageURL']
+
+      results << item
+    end
+
+    results
   end
 
-  def self.search(search_term)
+  def self.amazon_search(search_term)
 
-    iso_time = Time.now.iso8601.split("-")
-    day_ahead = iso_time[2].split("T")
-    iso_time[2] = (day_ahead.first.to_i + 1).to_s + "T" + day_ahead.last
-    iso_time = iso_time[0]+"-"+iso_time[1]+"-"+iso_time[2].gsub(/\:/, "%3A")+".000Z"
+    iso_time = "9999-12-31T23%3A59%3A59.000Z"
 
-    key = "5Yhv3A5LCzbBeDmNlzDW4j4fIdhfWNyVFEFF2IFd"
+    # iso_time = Time.now.iso8601.split("-")
+    # day_ahead = iso_time[2].split("T")
+    # iso_time[2] = (day_ahead.first.to_i + 1).to_s + "T" + day_ahead.last
+    # iso_time = iso_time[0]+"-"+iso_time[1]+"-"+iso_time[2]+".000Z"
+
+
+    key = ENV["AWSSecretKey"]
+    access_key= ENV["AWSAccessKeyId"]
     data = "GET
 webservices.amazon.com
 /onca/xml
-AWSAccessKeyId=AKIAJOST6FYFMTXDJ3NA&AssociateTag=giftrcom-20&Condition=All&Keywords=#{URI.escape(search_term)}&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=#{iso_time}&Version=2011-08-01"
+AWSAccessKeyId=#{access_key}&AssociateTag=giftrcom-20&Condition=All&Keywords=#{URI.escape(search_term)}&Operation=ItemSearch&ResponseGroup=Images%2CItemAttributes&SearchIndex=All&Service=AWSECommerceService&Timestamp=#{iso_time}&Version=2011-08-01"
 
     sig_code = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), key, data)).strip()
     # sig_code.gsub!(/\+/, "%2")
     # sig_code.gsub!(/\//, "%2")
     # sig_code.gsub!(/\=/, "%3D")
 
-    CGI.escape(sig_code)
-
     data = data.split("\n")
 
-    api_call = "http://"+data[1]+data[2]+"?"+data[3]+"&Signature="+sig_code
+    api_call = "http://"+data[1]+data[2]+"?"+data[3]+"&Signature="+CGI.escape(sig_code)
     HTTParty.get(api_call)
+
   end
 end
